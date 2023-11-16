12a18f55e27cda14261437f751ab8e43d4c9da2d
Louis Sloan <elizasloan005@gmail.com>
2023-11-16 02:08:58 -0600

    task 2 WIP

--- src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/AbstractEntity.java
@@ -8,11 +8,13 @@ import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 
 import java.util.Objects;
-
+@MappedSuperclass
 public abstract class AbstractEntity {
-
+    @GeneratedValue
+    @Id
     private int id;
-
+@NotNull
+@Size(min = 1, max = 200)
     private String name;
 
     public int getId() {
--- src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
+++ src/main/java/org/launchcode/techjobs/persistent/models/Employer.java
@@ -1,4 +1,19 @@
 package org.launchcode.techjobs.persistent.models;
 
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+@Entity
 public class Employer extends AbstractEntity {
+    @NotNull
+    @Size(min = 1, max = 200)
+    private String location;
+
+    public Employer() {
+    }
+
+    public String getLocation() {
+        return location;
+    }
 }
